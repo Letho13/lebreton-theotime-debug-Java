f99fe5b88c243394d08e71afefbe89288dbb985e
Theotime.Lebreton <theotime.lebreton@gmail.com>
2024-07-03 14:40:09 +0200

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,40 +4,58 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int dialatedPupilsCount = 0;		
 	
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+				
+		// Creation of a FileReader ton read the file
+		FileReader fileReader = new FileReader("C:\\Users\\Theotime\\Documents\\OPENCLASSROOMS\\Git\\lebreton-theotime-debug-Java\\Project02Eclipse\\symptoms.txt");
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		// Creation of a buffreader who use filereader
+		BufferedReader reader = new BufferedReader (fileReader);
+			String line = reader.readLine();
+
+			
+
 		while (line != null) {
-			i++;	// increment i
+		
+			
 			System.out.println("symptom from file: " + line);
+			
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++; // modification from headCount to headacheCount 
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			//correction "rush" in "rash"
+
+			else if (line.equals("rash")) { 
 				rashCount++;
+				System.out.println("number of rash: " + rashCount); //adding this line for rash count
 			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			else if (line.equals("dialated pupils")) { //correcting dialated pupils and equals instead of contain
+				dialatedPupilsCount++;
+				System.out.println("number of dialated pupils: " + dialatedPupilsCount); 
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();	// get another symptom 	----	Is it usefull ??
+			
 		}
-		
+		reader.close(); // closing the reading of bufferedReader
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + dialatedPupilsCount + "\n");
 		writer.close();
-	}
+		
+}
 }
