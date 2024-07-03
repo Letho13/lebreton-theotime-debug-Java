6958b42670c4514a2b0cd587420b3b532b93b74b
Theotime.Lebreton <theotime.lebreton@gmail.com>
2024-07-03 16:43:23 +0200

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,38 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+
+/**
+ * Simple brute force implementation
+ *
+ */
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+    
+    private String filePath;
+    
+    public WriteSymptomDataToFile (String filePath) {
+		this.filePath = filePath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
